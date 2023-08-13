e0e6990422fe7dd59de38e975fe9c78b86f17716
Jonathan Schneider <jkschneider@gmail.com>
2023-08-12 20:19:23 -0400

    EmbeddingModelClientTest doesn't work on CI

--- src/test/java/io/moderne/ai/EmbeddingModelClientTest.java
+++ src/test/java/io/moderne/ai/EmbeddingModelClientTest.java
@@ -1,9 +1,11 @@
 package io.moderne.ai;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisabledIfEnvironmentVariable(named = "CI", matches = "true")
 public class EmbeddingModelClientTest {
 
     @Test
