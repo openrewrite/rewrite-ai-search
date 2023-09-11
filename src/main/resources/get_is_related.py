83c8c08f24857d5a5680a17bd3ea7131cbe7cc95
juju <justine.gehring@gmail.com>
2023-09-11 12:13:23 -0400

    Changing HF_CACHE path

--- src/main/resources/get_is_related.py
+++ src/main/resources/get_is_related.py
@@ -15,10 +15,10 @@
 #
 
 import os
-os.environ["XDG_CACHE_HOME"]=os.path.expanduser("~") + "/.hfcache/HF_CACHE"
-os.environ["HF_HOME"]=os.path.expanduser("~") + "/.hfcache/HF_CACHE/huggingface"
-os.environ["HUGGINGFACE_HUB_CACHE"]=os.path.expanduser("~") + "/.hfcache/HF_CACHE/huggingface/hub"
-os.environ["TRANSFORMERS_CACHE"]=os.path.expanduser("~") + "/.hfcache/HF_CACHE/huggingface"
+os.environ["XDG_CACHE_HOME"]=os.path.expanduser("~") + "/HF_CACHE"
+os.environ["HF_HOME"]=os.path.expanduser("~") + "/HF_CACHE/huggingface"
+os.environ["HUGGINGFACE_HUB_CACHE"]=os.path.expanduser("~") + "/HF_CACHE/huggingface/hub"
+os.environ["TRANSFORMERS_CACHE"]=os.path.expanduser("~") + "/HF_CACHE/huggingface"
 import torch #pytorch = 2.0.1
 from typing import List, Union, Dict
 from transformers import AutoModel, AutoTokenizer, logging # 4.29.2
