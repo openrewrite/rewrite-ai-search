94fa45488dbb8890b0f59d564027cba99bf7e2e4
Tim te Beek <tim@moderne.io>
2023-09-12 13:52:48 +0100

    Update build.gradle.kts to adopt rewrite-bom

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,9 @@ description = "Rewrite AI search."
 val rewriteBomVersion = rewriteRecipe.rewriteVersion.get()
 
 dependencies {
-    implementation(platform("org.openrewrite.recipe:rewrite-recipe-bom:${rewriteBomVersion}"))
-    implementation("org.openrewrite:rewrite-core:latest.release")
-    implementation("org.openrewrite:rewrite-java:latest.release")
+    implementation(platform("org.openrewrite:rewrite-bom:$rewriteBomVersion"))
+    implementation("org.openrewrite:rewrite-core")
+    implementation("org.openrewrite:rewrite-java")
     implementation("com.konghq:unirest-java:3.14.2")
     testRuntimeOnly("org.openrewrite:rewrite-java-17")
 }
