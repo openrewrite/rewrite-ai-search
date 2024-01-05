44530ea5a0f48eaae70bdf653eb48f7cede79620
juju <justine.gehring@gmail.com>
2024-01-05 15:03:36 -0500

    Collect embeddings for clustering classes or methods

---
+++ src/test/java/io/moderne/ai/search/GetCodeEmbeddingTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.moderne.ai.search;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import java.util.List;
+
+import static org.openrewrite.java.Assertions.java;
+
+@DisabledIfEnvironmentVariable(named = "CI", matches = "true")
+public class GetCodeEmbeddingTest implements RewriteTest {
+
+    @Test
+    void methods() {
+        rewriteRun( spec -> spec.recipe(new GetCodeEmbedding("methods")),
+          java("""
+                public class Foo {
+                    public void hello(){
+                        System.out.println("hello");
+                    }
+                }
+                """)
+        );
+    }
+
+    @Test
+    void classes() {
+        rewriteRun( spec -> spec.recipe(new GetCodeEmbedding("classes")),
+          java("""
+                public class Foo {
+                    public void hello(){
+                        System.out.println("hello");
+                    }
+                }
+                """)
+        );
+    }
+}
