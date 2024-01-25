3855f6ded72d4ee19a201f7aac6308c371c1bba3
juju <justine.gehring@gmail.com>
2024-01-25 15:01:26 -0500

    rebuild rec_agent

--- src/main/java/io/moderne/ai/AgentRecommenderClient.java
+++ src/main/java/io/moderne/ai/AgentRecommenderClient.java
@@ -68,7 +68,7 @@ public class AgentRecommenderClient {
             INSTANCE = new AgentRecommenderClient();
             if (INSTANCE.checkForUpRequest() != 200) {
                 String cmd = String.format("/usr/bin/python3 'import gradio\ngradio.'", MODELS_DIR);
-                String cmd_pip = "/usr/bin/python3 -m pip install moderne-recommendation-agent --index-url https://moderne-recommender:${AZURE_DEVOPS_PAT}@pkgs.dev.azure.com/moderneinc/moderne/_packaging/moderne-recommender/pypi/simple/";
+                String cmd_pip = "/usr/bin/python3 -m pip install moderne-recommendation-agent --index-url https://moderne-recommender:${AZURE_DEVOPS_PAT}@pkgs.dev.azure.com/moderneinc/moderne/_packaging/moderne-recommender/pypi/simple/ --force-reinstall --no-cache-dir";
                 String cmd_run = "/usr/bin/python3 -c 'import moderne-recommendation-agent;  moderne-recommendation-agent._run('','code', 1)";
                 Process proc_run = null;
                 Process proc_pip = null;
