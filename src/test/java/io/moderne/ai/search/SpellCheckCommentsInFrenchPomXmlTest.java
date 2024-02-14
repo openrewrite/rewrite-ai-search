7d737be372947be9a9f6b325d57745f2dcdf3a39
juju <justine.gehring@gmail.com>
2024-02-14 16:56:18 -0500

    remove import unused

--- src/test/java/io/moderne/ai/search/SpellCheckCommentsInFrenchPomXmlTest.java
+++ src/test/java/io/moderne/ai/search/SpellCheckCommentsInFrenchPomXmlTest.java
@@ -17,8 +17,6 @@ package io.moderne.ai.search;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
-import org.openrewrite.DocumentExample;
-
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 import static org.openrewrite.maven.Assertions.pomXml;
